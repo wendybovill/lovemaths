d6c74dbef8157d93ead2e83ef45742deb7324036
wendybovill <wendybovill@gmail.com>
2023-03-10 14:33:10 +0000

    added html operators changed id element to value for operator added value attribute to html element retrieved operator value for sum added increment functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,12 +50,11 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
-
-    runGame(calculatedAnswer[1]);
-
 }
 
 /**
@@ -66,16 +65,17 @@ function calculateCorrectAnswer() {
 
     let operand1 = parseInt(document.getElementById('operand1').innerText);
     let operand2 = parseInt(document.getElementById('operand2').innerText);
-    let operator = document.getElementById("operator").innerText;
+    let operator = document.getElementById("operator").value;
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtraction"];
-    } else if (operator === "x") {
+    } else if (operator === "*") {
         return [operand1 * operand2, "multiply"];
-    } else if (operator === "&divide;") {
-        return [operand1 / operand2, "divide"];
+    } else if (operator === "/") { 
+            return [operand1 / operand2, "divide"];
+        
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -83,40 +83,59 @@ function calculateCorrectAnswer() {
     return correctAnswer;
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+    
 }
 
 function displayAdditionQuestion(operand1, operand2) {
 
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "+";
+    document.getElementById('operator').innerHTML = "+";
+    document.getElementById('operator').value = "+";
+
     
 }
 
 function displaySubtractQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "-";
+    document.getElementById('operator').innerHTML = "-";
+    document.getElementById('operator').value = "-";
+
     
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').innerHTML = `&times;`;
+    document.getElementById('operator').innerHTML= "&times;";
+    document.getElementById('operator').value = "*";
+
     
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-
+    operand1 = operand1*operand2;
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').innerHTML = `&divide;`;
+    document.getElementById('operator').innerHTML = "&divide;";
+    document.getElementById('operator').value = "/";
     
 }
